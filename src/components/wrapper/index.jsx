bec5175df4f042d01a4cb0f188dc46b961633dc2
Ole M <olesnoen@gmail.com>
2023-05-28 16:47:20 +0200

    test

--- src/components/wrapper/index.jsx
+++ src/components/wrapper/index.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import Header from "../Header";
+import Header from "../Header/index";
 import Footer from "../Footer";
 function Wrapper() {
     return (
