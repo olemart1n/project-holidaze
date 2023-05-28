a671995a7b94d04a82929e965cc4eb58f282e8d3
Ole M <olesnoen@gmail.com>
2023-05-28 16:48:33 +0200

    troubleshooting

--- src/components/wrapper/index.jsx
+++ src/components/wrapper/index.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import Header from "../Header/index";
+import Header from "../Header/index.jsx";
 import Footer from "../Footer";
 function Wrapper() {
     return (
